a944967b54fb3b14704e152f46579410b339f680
Matheus Marques <matheus.marques@leadstation.com.br>
2022-09-07 19:52:08 -0300

    Added profile image

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,11 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name,:username,:email,:password,:password_confirmation)
+    params.require(:user).permit(:name,:username,:email,:password,:password_confirmation,:profile_image)
   end
 
   def account_update_params
-    params.require(:user).permit(:name,:username,:email,:password,:password_confirmation,:current_password)
+    params.require(:user).permit(:name,:username,:email,:password,:password_confirmation,:current_password,:profile_image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :tweets
   has_many :comments
+  has_one_attached :profile_image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
