af2810264e2269457cdca0582745324a939d49f2
Matheus Marques <matheus.marques@leadstation.com.br>
2022-09-08 12:11:33 -0300

    added profile for all users

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,6 +7,10 @@ class Tweet < ApplicationRecord
 
   validates :body, presence: true, unless: :tweet_id
 
+  after_create_commit{broadcast_append_to "tweets"}
+
+  after_update_commit{broadcast_append_to "tweets"}
+
   def tweet_type
     if tweet_id?
       "retweet"
